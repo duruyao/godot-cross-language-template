d9c7d4c15b247d07f83de4f711bbdf1511634eaa
Ryan Du <duruyao@gmail.com>
2026-04-23 12:41:28 +0800

    chore(extensions): fix function declaration in header file

--- src/extensions/bar/register_types.hpp
+++ src/extensions/bar/register_types.hpp
@@ -1,7 +1,7 @@
 #ifndef BAR_REGISTER_TYPES_HPP
 #define BAR_REGISTER_TYPES_HPP
 
-void initialize_gdextension_types();
-void uninitialize_gdextension_types();
+void initialize_gdextension_types(ModuleInitializationLevel);
+void uninitialize_gdextension_types(ModuleInitializationLevel);
 
 #endif // BAR_REGISTER_TYPES_HPP
